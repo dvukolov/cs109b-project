2cc9aef0bd6fe90919a88af326eafd0b8ecd8901
Dmitry Vukolov <dmvuko@gmail.com>
2020-04-25 14:27:25 +0300

    Changed the order of operations for installing GalSim

--- data/update-galsim.sh
+++ data/update-galsim.sh
@@ -7,15 +7,15 @@ set -e
 # Remove GalSim
 conda uninstall --force -y galsim
 
-# Get the source code with an increased cache size of 1000
-git clone --single-branch --branch sersic-cache https://github.com/dvukolov/GalSim.git
-pushd GalSim
-
 # Install dependencies
 sudo apt-get -qq update
 sudo apt-get install -y python3-dev libfftw3-dev libeigen3-dev pybind11-dev
 
+# Get the source code with an increased cache size of 1000
+git clone --single-branch --branch sersic-cache https://github.com/dvukolov/GalSim.git
+
 # Build and install GalSim
+pushd GalSim
 python setup.py install
 popd
 
