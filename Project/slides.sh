6bb0cf3bb20c26f06f2847f0fca3f833e0642b1f
Dmitry Vukolov <dmvuko@gmail.com>
2020-05-07 12:29:49 +0300

    Updated shell scripts for slide conversion

--- Project/slides.sh
+++ Project/slides.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jupyter nbconvert presentation.ipynb --to slides --TemplateExporter.exclude_input=True
+jupyter nbconvert report*.ipynb presentation*.ipynb --to slides --TemplateExporter.exclude_input=True
 # Include slide numbering:
 sed -i 's/\(transition: "slide",\)/\1 slideNumber: "c\/t",/' *.slides.html
 
