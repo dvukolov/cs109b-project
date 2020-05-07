56383aabaef238872e3e7180b0bfe093ec309cd4
Dmitry Vukolov <dmvuko@gmail.com>
2020-05-07 15:25:22 +0300

    Don't convert the report to slides

--- Project/watch.sh
+++ Project/watch.sh
@@ -3,5 +3,5 @@
 # Update the slides whenever the notebooks change.
 # Requires `entr`, available for Linux and Mac.
 
-ls report*.ipynb presentation*.ipynb | entr -r ./slides.sh 
+ls presentation*.ipynb | entr -r ./slides.sh 
 
