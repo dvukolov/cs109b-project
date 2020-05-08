7ec03feeac67cd8e76525e17c4e09f2be318c2fc
Dmitry Vukolov <dmvuko@gmail.com>
2020-05-09 02:31:26 +0300

    Presentation and report updates

--- Project/slides.sh
+++ Project/slides.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jupyter nbconvert presentation*.ipynb --to slides --TemplateExporter.exclude_input=True
+jupyter nbconvert presentation*.ipynb --to slides --TemplateExporter.exclude_input=True #--SlidesExporter.reveal_transition=none
 # Include slide numbering:
 sed -i 's/\(transition: "slide",\)/\1 slideNumber: "c\/t",/' *.slides.html
 
